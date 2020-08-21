2adadf752927f85d6a74ffa993f02727ba4681bf
Twan Box <twanbox@gmail.com>
2020-08-21 23:18:22 +0200

    routes set up

--- ecommerce-project/src/App.js
+++ ecommerce-project/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/afrekenen" >
+            <h1>Afrekenen</h1>
+          </Route>
+          <Route path="/inloggen" >
+            <h1>Inloggen</h1>
+          </Route>
+          <Route path="/" >
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
